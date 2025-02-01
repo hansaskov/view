8f9eb51a76070268f3a31598d520454a67320e81
Hans Askov <hans@askov.dk>
2025-02-01 12:53:02 +0100

    Create data dir for sqlite

--- src/db/sqlite.ts
+++ src/db/sqlite.ts
@@ -1,8 +1,12 @@
+
 import { Database } from "bun:sqlite";
 import { env } from "elysia";
+import { join } from "node:path"
 
+const DATA_PATH = env.DATA_PATH ?? "./data";
 const SQLITE = env.SQLITE ?? "sqlite.db";
 
-console.log(`Running sqlite as ${SQLITE}`)
+const SQLITE_PATH = SQLITE === ":memory:" ? ":memory:" : join(DATA_PATH, SQLITE);
 
-export const sqlite = new Database(SQLITE);
+console.log(`Running sqlite as ${SQLITE_PATH}`);
+export const sqlite = new Database(SQLITE_PATH);
