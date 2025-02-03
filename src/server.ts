8ea467909f2179af37c570b72dfde025603e325f
Hans Askov <hans@askov.dk>
2025-02-03 23:29:38 +0100

    Update login page

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,8 @@
 import { Elysia } from "elysia";
 import homepage from "./pages/index.html";
+import login from "./pages/login.html";
 
 export const app = new Elysia()
+	.get("/favicon.ico", Bun.file("./public/favicon.ico"))
 	.get("/", homepage)
-	.get("/favicon.ico", Bun.file("./public/favicon.ico"));
+	.get("/login", login);
