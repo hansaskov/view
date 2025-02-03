a52dc04d0036e570cf6266f409cb0617a66a5c59
Hans Askov <hans@askov.dk>
2025-02-03 21:22:22 +0100

    Add suppoert for daisy ui

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,6 @@
-import { type User, users } from "$collections/users";
 import { Elysia } from "elysia";
 import homepage from "./pages/index.html";
 
 export const app = new Elysia()
 	.get("/", homepage)
-	.get("/users", async () => await users.queries.selectAll())
-	.get(
-		"/hans",
-		async () =>
-			await users.queries.insert({ name: "hans", email: "hans@asov.dk" }),
-	);
+	.get("/favicon.ico", Bun.file("./public/favicon.ico"));
