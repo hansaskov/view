3f741b7da12d88167c17fbe5ea41be65fa014bc9
Hans Askov <hans@askov.dk>
2025-02-06 18:49:22 +0100

    Add experimental middleware

--- src/auth/better-auth.ts
+++ src/auth/better-auth.ts
@@ -1,9 +1,11 @@
+import type { Session } from "$collections/session";
+import type { User } from "$collections/users";
 import { db } from "$db/drizzle";
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { reddit } from "better-auth/social-providers";
 import { resolve } from "bun";
-import Elysia, { redirect } from "elysia";
+import Elysia, { error, redirect } from "elysia";
 
 export const auth = betterAuth({
 	database: drizzleAdapter(db, {
@@ -35,6 +37,33 @@ export const authMiddleware = new Elysia()
 	.derive(({ request }) => userMiddleware(request))
 	.as("plugin");
 
+export const authMiddleware2 = new Elysia()
+	.derive(({ request }) => userMiddleware(request))
+	.macro({
+		user: (enable: boolean) => ({
+			beforeHandle: async ({ user, session }) => {
+				if (enable && (!user || !session)) {
+					return error("Unauthorized");
+				}
+			},
+			resolve: async ({ user, session }) => {
+				if (enable === false) {
+					return;
+				}
+
+				user = user as User;
+				session = session as Session;
+
+				return { user, session };
+			},
+		}),
+	})
+	.as("plugin");
+
 export const authRoutes = new Elysia({ prefix: "/api/auth/*" })
 	.post("/", auth.handler)
-	.get("/", auth.handler);
+	.get("/", auth.handler)
+	.use(authMiddleware2)
+	.get("/", ({ user }) => "", {
+		user: false,
+	});
