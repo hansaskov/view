17648018577bdd14ebc8c3ce8c55247fbfa6989b
Hans Askov <hans@askov.dk>
2025-07-02 21:15:50 +0200

    fix check

--- src/client/lib/api.ts
+++ src/client/lib/api.ts
@@ -2,5 +2,3 @@ import { treaty } from "@elysiajs/eden"
 import type { App } from "../../server"
 
 export const api = treaty<App>("http://127.0.0.1:3000").api
-
-export const fetchPosts = api.names.get
